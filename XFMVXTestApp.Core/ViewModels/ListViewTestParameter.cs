2258887c1910e13fc9595ec73b16f146e0f790ab
Tim Johnson <tjohnson@aptos.com>
2021-02-11 19:52:13 -0500

    remainder of files i missed in that PR..

--- XFMVXTestApp.Core/ViewModels/ListViewTestParameter.cs
+++ XFMVXTestApp.Core/ViewModels/ListViewTestParameter.cs
@@ -4,10 +4,12 @@ namespace XFMVXTestApp.Core.ViewModels
     public class ListViewTestParameter
     {
         public int NumberOfItems { get; set; }
+        public bool Expanded { get; set; }
 
-        public ListViewTestParameter(int numberOfItems)
+        public ListViewTestParameter(int numberOfItems, bool expanded = true)
         {
             NumberOfItems = numberOfItems;
+            Expanded = expanded;
         }
     }
 }
